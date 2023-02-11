e03ead30558209efbcf9e7f1c663ebba3e3f3735
brightrock <ricekent@163.com>
2023-02-11 10:20:04 +0800

    feat: csv2db add formatTitle

--- postgres/csv2db.py
+++ postgres/csv2db.py
@@ -6,6 +6,10 @@ from pathlib import Path
 from sqlalchemy import create_engine
 
 
+def formatTitle(title) -> str:
+    return title.lower()
+
+
 def toPsql(
     csv: Path,
     name: str,
@@ -14,20 +18,23 @@ def toPsql(
     db: str,
     host="localhost",
     port=5432,
-    add_id=False,
-    append=False,
+    add_id: bool = typer.Option(False, "--add-id"),
+    replace_table: bool = typer.Option(False, "--replace"),
+    format_title: bool = typer.Option(False, "--format-title"),
     sep=",",
 ):
     engine = create_engine(
         f"postgresql+psycopg2://{username}:{password}:@{host}:{port}/{db}"
     )
     df = pd.read_csv(csv, sep=sep)
+    if format_title:
+        df.columns = [formatTitle(each) for each in df.columns]
     if add_id:
         df.loc[:, "id"] = [each + 1 for each in df.index]
-    if append:
-        df.to_sql(name, engine, if_exists="append", index=False, chunksize=10000)
-    else:
+    if replace_table:
         df.to_sql(name, engine, if_exists="replace", index=False, chunksize=10000)
+    else:
+        df.to_sql(name, engine, if_exists="append", index=False, chunksize=10000)
 
 
 if __name__ == "__main__":
