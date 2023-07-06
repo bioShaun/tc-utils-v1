36222d8187951573c23f6a010603bc55745cdfb9
brightrock <ricekent@163.com>
2023-07-06 15:07:15 +0800

    feat: maf to af

--- exome/siteStatsFromVcf.py
+++ exome/siteStatsFromVcf.py
@@ -13,13 +13,12 @@ def get_allele_stats(record: vcfpy.Record) -> List[float]:
     gt_count = Counter(gt_list)
     missing_count = gt_count.get("./.", 0)
     valid_count = len(gt_list) - missing_count
-    ref_count = gt_count.get("0/0", 0)
+    alt_count = gt_count.get("1/1", 0)
     het_count = gt_count.get("0/1", 0)
     missing_rate = round(missing_count / len(gt_list), 3)
     het_rate = round(het_count / valid_count, 3)
-    ref_rate = (ref_count + het_count * 0.5) / valid_count
-    maf = round(min(ref_rate, 1 - ref_rate), 3)
-    return [missing_rate, het_rate, maf]
+    af = (alt_count + het_count * 0.5) / valid_count
+    return [missing_rate, het_rate, af]
 
 
 def main(vcf: Path, site_stats: Path) -> None:
@@ -27,10 +26,10 @@ def main(vcf: Path, site_stats: Path) -> None:
         reader = vcfpy.Reader.from_path(vcf)
         for record in tqdm(reader):
             if record:
-                missing_rate, het_rate, maf = get_allele_stats(record)
+                missing_rate, het_rate, af = get_allele_stats(record)
                 alt = ",".join([each.value for each in record.ALT])
                 site_inf.write(
-                    f'{record.CHROM},{record.POS},"{record.REF},{alt}",{missing_rate},{het_rate},{maf}\n'
+                    f'{record.CHROM},{record.POS},"{record.REF},{alt}",{missing_rate},{het_rate},{af}\n'
                 )
 
 
