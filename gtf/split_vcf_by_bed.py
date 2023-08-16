62df37edbbc5e54d7ace906a380c200ca8496eda
brightrock <ricekent@163.com>
2023-08-16 16:19:58 +0800

    feat: add split vcf

---
+++ gtf/split_vcf_by_bed.py
@@ -0,0 +1,37 @@
+import gzip
+from pathlib import Path
+
+import typer
+
+
+def main(bed: Path, vcf: Path):
+    chr_dict = dict()
+
+    with open(bed) as bed_inf:
+        for eachline in bed_inf:
+            eachline_inf = eachline.strip().split()
+            chrom = eachline_inf[0]
+            start = int(eachline_inf[1])
+            end = int(eachline_inf[2])
+            split_chr = eachline_inf[3]
+            chr_dict.setdefault(chrom, {})[(start, end)] = split_chr
+
+    with gzip.open(vcf, "rt") as gtf_info:
+        for eachline in gtf_info:
+            eachline = eachline.strip()
+            if eachline.startswith("#"):
+                print(eachline)
+                continue
+            output_line = eachline.split("\t")
+            chrom, pos = output_line[:2]
+            if chrom in chr_dict:
+                for each_inter in chr_dict[chrom]:
+                    if int(pos) > each_inter[0] and int(pos) <= each_inter[1]:
+                        output_line[0] = chr_dict[chrom][each_inter]
+                        output_line[1] = str(int(pos) - each_inter[0])
+            output_str = "\t".join(output_line)
+            print(output_str)
+
+
+if __name__ == "__main__":
+    typer.run(main)
