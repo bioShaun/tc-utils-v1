825f53e8e2ef44ccc1b900ee6d3a2ab07700cf89
brightrock <ricekent@163.com>
2023-08-18 16:19:54 +0800

    feat: add cov

--- exome/variantsStats.py
+++ exome/variantsStats.py
@@ -1,5 +1,6 @@
 from io import StringIO
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 import typer
@@ -87,14 +88,45 @@ def indelStats(indel_stats: Path) -> pd.DataFrame:
     return merged_df
 
 
-def sampleStats(snp_stats: Path, indel_stats: Path, out_file: Path):
+def extract_coverage_stats(alignment_stats: Path, region_size: int) -> List[float]:
+    out_stats = [0.0, 0.0]
+    with alignment_stats.open() as align_info:
+        for each_line in align_info:
+            if "bases mapped (cigar)" in each_line:
+                alignment_length = int(each_line.split("\t")[2])
+                out_stats[0] = alignment_length / region_size
+            if "percentage of target genome with coverage" in each_line:
+                coverage = float(each_line.split("\t")[2])
+                out_stats[1] = coverage
+    return out_stats
+
+
+def coverageStats(alignment_dir: Path, region_size: int) -> pd.DataFrame:
+    cov_data = []
+    for cov_file in alignment_dir.glob('*/*.coverage.stat')
+        sample_name = cov_file.parent.name
+        cov_percent, cov_depth = extract_coverage_stats(cov_file, region_size)
+        cov_data.append([sample_name, cov_percent, cov_depth])
+    cov_df = pd.DataFrame(cov_data, columns=["Sample", "Mean Sequencing Depth", "Coverage >= 1x(%)"])
+    cov_df = cov_df.set_index("Sample")
+    multi_columns = [[""] * len(cov_df.columns), list(cov_df.columns)]
+    cov_df.columns = pd.MultiIndex.from_arrays(multi_columns)
+    return cov_df
+
+def get_region_size(bed_file: Path) -> int:
+    bed_df = pd.read_table(bed_file, header=None, names=["chrom", "start", "end"])
+    bed_df['region_size'] = bed_df["end"] - bed_df["start"]
+    return bed_df['region_size'].sum()
+
+def sampleStats(snp_stats: Path, indel_stats: Path, region_bed: Path, out_file: Path):
     snp_df = snpStats(snp_stats)
     indel_df = indelStats(indel_stats)
+    region_size = get_region_size(region_bed)
+    cov_df = coverageStats(snp_stats.parent, region_size)
     merged_df = pd.merge(snp_df, indel_df, left_index=True, right_index=True)
+    merged_df = pd.merge(cov_df, merged_df, left_index=True, right_index=True)
     merged_df.reset_index(col_level=1, inplace=True)
-    merged_df.to_csv(
-        out_file,
-    )
+    merged_df.to_csv(out_file, index=False)
 
 
 if __name__ == "__main__":
