af5d3a4154067b8d5eff3f86491cc89f9a49eaf1
brightrock <ricekent@163.com>
2023-09-28 15:37:38 +0800

    fix bug

--- gtf/add-gene-id.py
+++ gtf/add-gene-id.py
@@ -3,15 +3,15 @@ import typer
 
 
 def main(gtf: Path) -> None:
-    out_gtf = gtf.with_suffix("addGene.gtf")
+    out_gtf = gtf.with_suffix(".addGene.gtf")
     with out_gtf.open("w") as out:
         with gtf.open("r") as in_gtf:
             for line in in_gtf:
                 if "gene_id" in line:
                     out.write(line)
                 else:
-                    gene_id = line.strip().split()[:-1]
-                    out.write(f"{line.strip()} gene_id {gene_id}")
+                    gene_id = line.strip().split()[-1]
+                    out.write(f"{line.strip()} gene_id {gene_id}\n")
 
 
 if __name__ == "__main__":
--- gtf/renameChrom.py
+++ gtf/renameChrom.py
@@ -1,5 +1,5 @@
 import typer
-
+import csv
 from Bio import SeqIO
 import pandas as pd
 
@@ -7,7 +7,7 @@ from pathlib import Path
 
 gff_columns = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
 
-def main(genome: Path, gff: Path, chrom_map: Path) -> None
+def main(genome: Path, gff: Path, chrom_map: Path) -> None:
     genome_list = []
     chrom_map_dict = {}
     with open(chrom_map) as map_inf:
@@ -28,7 +28,7 @@ def main(genome: Path, gff: Path, chrom_map: Path) -> None
     gff_df = gff_df.merge(chrom_map_df)
     out_gff_name = f'rename.{gff.name}'
     out_gff_path = gff.parent / out_gff_name
-    gff_df.to_csv(out_gff_path, sep='\t', header=False, columns=['new_seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute'], index=False)
+    gff_df.to_csv(out_gff_path, sep='\t', header=False, columns=['new_seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute'], index=False, quoting=csv.QUOTE_NONE)
 
 
 if __name__ == "__main__":
