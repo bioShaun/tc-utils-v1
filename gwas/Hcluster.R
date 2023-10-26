4104061bf0eedbf684a474118c013fc7f2c58001
brightrock <ricekent@163.com>
2023-10-26 15:25:00 +0800

    feat: add pca plot

---
+++ gwas/Hcluster.R
@@ -0,0 +1,39 @@
+
+suppressMessages(library(gdsfmt))
+suppressMessages(library(SNPRelate))
+suppressMessages(library(argparser))
+
+options(stringsAsFactors = F)
+
+p <- arg_parser("vcf Hcluster")
+p <- add_argument(
+  p, "--vcf",
+  help = "vcf"
+)
+p <- add_argument(
+  p, "--outdir",
+  help = "output dir"
+)
+
+argv <- parse_args(p)
+
+
+#vcf to GDS
+snpgdsVCF2GDS(argv$vcf, "my.gds")
+genofile <- snpgdsOpen("my.gds")
+
+#dendogram
+dissMatrix  <-  snpgdsDiss(genofile,snp.id=NULL, autosome.only=TRUE,remove.monosnp=TRUE, maf=NaN, missing.rate=NaN, num.thread=10, verbose=TRUE)
+snpHCluster <-  snpgdsHCluster(dissMatrix, sample.id=sample.id[sample.id != DZ55-2018], need.mat=TRUE)
+cutTree <- snpgdsCutTree(snpHCluster, z.threshold=15, outlier.n=5, n.perm = 5000, samp.group=NULL,col.outlier=red, col.list=NULL, pch.outlier=4, pch.list=NULL,label.H=FALSE, label.Z=TRUE, verbose=TRUE)
+
+sample_cluster <- data.frame(sample_id=cutTree$sample.id, group=cutTree$samp.group)
+
+group_path = file.path(argv$outdir, "sample.group.tsv")
+
+write.table(sample_cluster, file=group_path, sep="\t", quote=F,row.names=F)
+
+#svg(filename = "diff.snp.hcluster.svg",width = 10,height = 16)
+#snpgdsDrawTree(cutTree, main = "",edgePar=list(col=rgb(0.5,0.5,0.5,0.75),t.col="black"),
+#               y.label.kinship=T,leaflab="perpendicular", y.label=0.05)
+#dev.off()
---
+++ gwas/pca.R
@@ -0,0 +1,47 @@
+suppressMessages(library(RColorBrewer))
+suppressMessages(library(ggplot2))
+suppressMessages(library(stringr))
+
+options(stringsAsFactors = F)
+
+p <- arg_parser("vcf pca")
+p <- add_argument(
+  p, "--plink_prefix",
+  help = "plink prefix"
+)
+p <- add_argument(
+  p, "--sample_group",
+  help = "sample group"
+)
+p <- add_argument(
+  p, "--outdir",
+  help = "outdir"
+)
+
+argv <- parse_args(p)
+
+df <- read.delim(paste(argv$plink_prefix, 'eigenvec', sep="."), sep=" ", header=F, col.names = c("id", "sample_id", "PC1", "PC2", "PC3", "PC4", "PC5"))
+eigenval <- read.delim(paste(argv$plink_prefix, 'eigenval', sep="."), header = F)
+
+pca.explained <- eigenval$V1 / sum(eigenval$V1)
+
+group.df <- read.delim(argv$sample_group)
+
+plot_df <- merge(df, group.df, by = "sample_id")
+groupNumber <- length(unique(group.df$group))
+
+plot.colors <- brewer.pal(9, 'Set1')
+group.colors <- colorRampPalette(plot.colors)(groupNumber)
+
+xlab_text <- str_c('PCA1 ', '(', round(pca.explained[1] * 100,2), '%)')
+ylab_text <- str_c('PCA2 ', '(', round(pca.explained[2] * 100,2), '%)')
+
+
+p <- ggplot(plot_df, aes(x=PC1, y=PC2, color=group)) + geom_point() + theme_classic() + 
+  theme(axis.title = element_text(size=rel(1.2)), axis.text = element_text(size = rel(1))) +
+  scale_color_manual(values = group.colors) +
+  guides(color=guide_legend(title="")) + xlab(xlab_text) + ylab(ylab_text)
+p
+
+ggsave(file.path(argv$outdir, 'pca.pdf'), width = 10, height = 8)
+ggsave(file.path(argv$outdir, 'pca.png'), width = 10, height = 8)
