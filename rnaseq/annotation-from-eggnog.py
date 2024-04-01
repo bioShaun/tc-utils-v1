3b50d15f1ae001f7f80d131944cbcfc18e6d04a6
brightrock <ricekent@163.com>
2024-04-01 10:30:17 +0800

    fix: reset index

--- rnaseq/annotation-from-eggnog.py
+++ rnaseq/annotation-from-eggnog.py
@@ -39,6 +39,7 @@ def gtf2gene_locations(gtf_path: Path) -> Tuple[pd.DataFrame, pd.DataFrame]:
             + "|"
             + locations["Strand"]
         )
+        locations = locations.reset_index()
         return tr2gene, locations
     raise ValueError(f"{gtf_path} is not a valid GTF file")
 
