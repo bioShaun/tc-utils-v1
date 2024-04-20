3a31381b96236c2f57d3bef50767116954e8e03d
brightrock <ricekent@163.com>
2024-04-20 17:07:31 +0800

    fix: use cols

--- panel/realign.py
+++ panel/realign.py
@@ -7,15 +7,19 @@ FLANK_SIZE = 60
 
 
 def main(paf: Path, id_map: Path) -> None:
-    paf_df = pd.read_table(paf, header=None)
-    paf_df.sort_values([11, 9], ascending=False, inplace=True)
-    paf_df.drop_duplicates(subset=[0], inplace=True)
-    filter_df = paf_df[paf_df[9] > FLANK_SIZE].copy()
-    filter_df["pos"] = filter_df[7] + 60 + 1 - filter_df[2]
-    filter_df["id"] = filter_df.apply(lambda x: f'{x[5]}_{x["pos"]}', axis=1)
-    filter_df.rename(columns={0: "old_id"}, inplace=True)
+    paf_df = pd.read_table(
+        paf,
+        header=None,
+        usecols=[0, 2, 5, 7, 9, 11],
+        names=["id", "probe_start", "chrom", "match_start", "match_length", "mapq"],
+    )
+    paf_df.sort_values(["mapq", "match_length"], ascending=False, inplace=True)
+    paf_df.drop_duplicates(subset=["id"], inplace=True)
+    filter_df = paf_df[paf_df["match_length"] > FLANK_SIZE].copy()
+    filter_df["pos"] = filter_df["match_start"] + 60 + 1 - filter_df["probe_start"]
+    filter_df["new_id"] = filter_df.apply(lambda x: f'{x["chrom"]}_{x["pos"]}', axis=1)
     filter_df.to_csv(
-        id_map, header=False, index=False, columns=["old_id", "id"], sep="\t"
+        id_map, header=False, index=False, columns=["id", "new_id"], sep="\t"
     )
 
 
