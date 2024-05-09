adefb1bcb5779b04eb96c8f0fc0211e804482e27
brightrock <ricekent@163.com>
2024-05-09 16:18:50 +0800

    feat: filter no variant vcf support non gz file

--- exome/filterNoVariantVcf.py
+++ exome/filterNoVariantVcf.py
@@ -1,25 +1,49 @@
 import gzip
-import typer
+from enum import Enum
 from pathlib import Path
+from typing import TextIO
+
+import typer
 
 
-def main(vcf: Path, out_vcf: Path, va_type: str = "snp") -> None:
-    out_vcf_inf = out_vcf.open("w")
-    with gzip.open(vcf, "rt") as vcf_info:
-        for eachline in vcf_info:
-            if eachline.startswith("#"):
-                out_vcf_inf.write(eachline)
+class VaType(str, Enum):
+    SNP = "snp"
+    INDEL = "indel"
+
+
+def filter_no_variant_vcf(
+    input_vcf: Path, output_vcf: Path, variant_type: VaType = VaType.SNP
+) -> None:
+    """
+    Filter a VCF file and write the filtered results to an output VCF file.
+    """
+
+    with open_vcf(input_vcf) as input_vcf_file, output_vcf.open("w") as output_vcf_file:
+        for line in input_vcf_file:
+            if line.startswith("#"):
+                output_vcf_file.write(line)
                 continue
-            eachline_info = eachline.split("\t")
-            if eachline_info[4] == ".":
-                if va_type == "snp":
-                    if len(eachline_info[3]) > 1:
-                        continue
-                else:
-                    if len(eachline_info[3]) == 1:
-                        continue
-            out_vcf_inf.write(eachline)
+
+            columns = line.split("\t")
+            if columns[4] == ".":
+                if variant_type == VaType.SNP and len(columns[3]) > 1:
+                    continue
+                if variant_type == VaType.INDEL and len(columns[3]) == 1:
+                    continue
+
+            output_vcf_file.write(line)
+
+
+def open_vcf(vcf_path: Path) -> TextIO:
+    """
+    Open a VCF file. If the file is compressed with gzip, use the gzip module to open it.
+    """
+
+    if vcf_path.suffix == ".gz":
+        return gzip.open(vcf_path, "rt")
+    else:
+        return vcf_path.open("r")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(filter_no_variant_vcf)
