8d2b4541d81f588aefc7efd34095f059313478a6
brightrock <ricekent@163.com>
2024-06-18 15:15:20 +0800

    feat: add filter blast probe

---
+++ panel/filter-blast-probes.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pandas as pd
+import typer
+
+
+def main(
+    blast_dir: Path, out_file: Path, min_len: int = 30, max_match_count: int = 3
+) -> None:
+    df_list = [
+        pd.read_table(each, usecols=[0, 2, 3], names=["id", "identity", "match_len"])
+        for each in blast_dir.glob("./*")
+    ]
+    merged_df = pd.concat(df_list)
+    merged_df = merged_df[merged_df["match_len"] >= min_len]
+    id_count_df = merged_df["id"].value_counts()
+    filter_id_df = id_count_df[id_count_df <= max_match_count].reset_index()
+    filter_id_df.columns = ["id", "blast_match"]
+    filter_id_df.to_csv(out_file, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
