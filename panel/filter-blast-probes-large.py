1f88d00be82b23fc3e69db4d26086dc17cffa381
brightrock <ricekent@163.com>
2024-07-03 16:59:19 +0800

    fix: read table

--- panel/filter-blast-probes-large.py
+++ panel/filter-blast-probes-large.py
@@ -15,7 +15,9 @@ def main(
     result_df = pd.DataFrame()
 
     for n, blast_file in enumerate(tqdm(blast_files)):
-        blast_df = pd.read_table(blast_file, usecols=["id", "identity", "match_len"])
+        blast_df = pd.read_table(
+            blast_file, usecols=[0, 2, 3], names=["id", "identity", "match_len"]
+        )
         blast_df = blast_df[blast_df["match_len"] >= min_match_length]
         id_count_df = blast_df["id"].value_counts()
         filter_id_df = id_count_df[id_count_df <= max_match_count].reset_index()
