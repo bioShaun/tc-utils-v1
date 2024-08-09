c167efcb9b34dff35cede52b65ab595a64c7cee8
brightrock <ricekent@163.com>
2024-08-09 17:06:34 +0800

    feat: sort probe id

--- panel/add-probe-id.py
+++ panel/add-probe-id.py
@@ -7,6 +7,7 @@ import typer
 def main(gt: Path, id_map: Path, out: Path):
     gt_df = pd.read_table(gt)
     id_map_df = pd.read_table(id_map, header=None, names=["probe_id", "id"])
+    id_map_df.sort_values("probe_id", inplace=True)
     gt_df["id"] = gt_df["CHROM"].str.cat(gt_df["POS"].astype(str), sep="_")
     probe_gt_df = id_map_df.merge(gt_df).drop("id", axis=1)
     probe_gt_df.to_csv(out, sep="\t", index=False)
