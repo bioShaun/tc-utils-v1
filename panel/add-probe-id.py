24e608b84f582c774f00875dab0a5e7d33892294
brightrock <ricekent@163.com>
2024-08-09 17:05:15 +0800

    fix: merge order

--- panel/add-probe-id.py
+++ panel/add-probe-id.py
@@ -8,7 +8,7 @@ def main(gt: Path, id_map: Path, out: Path):
     gt_df = pd.read_table(gt)
     id_map_df = pd.read_table(id_map, header=None, names=["probe_id", "id"])
     gt_df["id"] = gt_df["CHROM"].str.cat(gt_df["POS"].astype(str), sep="_")
-    probe_gt_df = gt_df.merge(id_map_df).drop("id", axis=1)
+    probe_gt_df = id_map_df.merge(gt_df).drop("id", axis=1)
     probe_gt_df.to_csv(out, sep="\t", index=False)
 
 
