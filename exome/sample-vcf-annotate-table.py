b39431eafb96322ebd71d7008285f66e783af3f3
brightrock <ricekent@163.com>
2024-09-09 15:40:36 +0800

    feat: 将snpeff注释的vcf转换成excel表

---
+++ exome/sample-vcf-annotate-table.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+import delegator
+import pandas as pd
+import typer
+
+EXTRACT_VCF_FILEDS = (
+    'CHROM POS REF "ANN[*].ALLELE" '
+    '"ANN[*].IMPACT" "ANN[*].EFFECT" "ANN[*].GENEID"  '
+    '"ANN[*].HGVS_C" "ANN[*].HGVS_P" GEN[0].GT GEN[0].AD'
+)
+
+
+COLUMN_MAP = {
+    "ANN[*].ALLELE": "ALT",
+    "ANN[*].IMPACT": "IMPACT",
+    "ANN[*].EFFECT": "EFFECT",
+    "ANN[*].GENEID": "GENEID",
+    "ANN[*].HGVS_C": "HGVS_C",
+    "ANN[*].HGVS_P": "HGVS_P",
+    "GEN[0].GT": "GT",
+    "GEN[0].AD": "AD",
+}
+
+
+def extract_snpeff_anno(
+    vcf_path: Path, annotation_file: Path, snpeff_dir: Path, force: bool
+) -> None:
+    if force or not annotation_file.is_file():
+        extract_cmd = (
+            f"zcat {vcf_path} | "
+            f"{snpeff_dir}/scripts/vcfEffOnePerLine.pl | "
+            f"java -jar {snpeff_dir}/SnpSift.jar extractFields - {EXTRACT_VCF_FILEDS} | "
+            f"gzip > {annotation_file}"
+        )
+        delegator.run(extract_cmd)
+
+
+def main(
+    vcf_file: Path,
+    sample_name: str,
+    snpeff_dir: Path,
+    out_file: Path,
+    force: bool = False,
+):
+    annotation_file = vcf_file.with_suffix(".raw.table.tsv.gz")
+    extract_snpeff_anno(
+        vcf_path=vcf_file,
+        snpeff_dir=snpeff_dir,
+        force=force,
+        annotation_file=annotation_file,
+    )
+    df = pd.read_table(annotation_file)
+    df.rename(columns=COLUMN_MAP, inplace=True)
+    df["Sample"] = sample_name
+    df["Ref_Depth"] = df["AD"].map(lambda x: int(x[0]))
+    df["Alt_Depth"] = df["AD"].map(lambda x: int(x[1]))
+    df.to_excel(out_file, index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
