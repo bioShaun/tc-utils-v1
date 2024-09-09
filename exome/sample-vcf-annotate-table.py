df348146692985399db553335528e02d9575bf84
brightrock <ricekent@163.com>
2024-09-09 16:05:15 +0800

    feat: use gatk to extract sample vcf

--- exome/sample-vcf-annotate-table.py
+++ exome/sample-vcf-annotate-table.py
@@ -37,6 +37,19 @@ def extract_snpeff_anno(
         delegator.run(extract_cmd)
 
 
+def get_sample_names(vcf_file: Path) -> list:
+    cmd = f"bcftools query -l {vcf_file}"
+    return delegator.run(cmd).out.strip().split("\n")
+
+
+def selectSampleVariants(
+    vcf_file: Path, sample_name: str, out_file: Path, force: bool = False
+) -> None:
+    if force or not out_file.is_file():
+        cmd = f"gatk SelectVariants -V {vcf_file} --exclude-non-variants --sample-name {sample_name} -O {out_file}"
+        delegator.run(cmd)
+
+
 def main(
     vcf_file: Path,
     sample_name: str,
@@ -44,22 +57,31 @@ def main(
     out_file: Path,
     force: bool = False,
 ):
-    annotation_file = vcf_file.with_suffix(".raw.table.tsv.gz")
-    extract_snpeff_anno(
-        vcf_path=vcf_file,
-        snpeff_dir=snpeff_dir,
-        force=force,
-        annotation_file=annotation_file,
-    )
-    df = pd.read_table(annotation_file)
-    df.drop_duplicates(subset=["CHROM", "POS", "REF", "ALT"], inplace=True)
-    df.rename(columns=COLUMN_MAP, inplace=True)
-    df["Sample"] = sample_name
-    df["Ref_Depth"] = df["AD"].map(lambda x: int(x[0]))
-    df["Alt_Depth"] = df["AD"].map(lambda x: int(x[1]))
-    if out_file.suffix != ".csv":
-        raise ValueError(f"Output file should be .csv: {out_file}")
-    df.to_csv(out_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
+    sample_list = get_sample_names(vcf_file)
+    for sample in sample_list:
+        sample_vcf = vcf_file.with_suffix(f".{sample}.vcf.gz")
+        selectSampleVariants(
+            vcf_file=vcf_file,
+            sample_name=sample,
+            out_file=sample_vcf,
+            force=force,
+        )
+        annotation_file = sample_vcf.with_suffix(".raw.table.tsv.gz")
+        extract_snpeff_anno(
+            vcf_path=sample_vcf,
+            snpeff_dir=snpeff_dir,
+            force=force,
+            annotation_file=annotation_file,
+        )
+        df = pd.read_table(annotation_file)
+        df.drop_duplicates(subset=["CHROM", "POS", "REF", "ALT"], inplace=True)
+        df.rename(columns=COLUMN_MAP, inplace=True)
+        df["Sample"] = sample_name
+        df["Ref_Depth"] = df["AD"].map(lambda x: int(x[0]))
+        df["Alt_Depth"] = df["AD"].map(lambda x: int(x[1]))
+        if out_file.suffix != ".csv":
+            raise ValueError(f"Output file should be .csv: {out_file}")
+        df.to_csv(out_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
 
 
 if __name__ == "__main__":
