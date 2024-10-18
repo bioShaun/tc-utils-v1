cfe2ac866433ffa41d1b315047f43506cebdecbd
brightrock <ricekent@163.com>
2024-10-18 16:09:59 +0800

    feat: unkown strand to +

--- gtf/add-gene-id.py
+++ gtf/add-gene-id.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import re
+from pathlib import Path
+
 import typer
 
 
@@ -10,17 +11,21 @@ def main(gtf: Path) -> None:
         for line in in_gtf:
             line_inf = line.strip().split("\t")
             out_inf = line
-            #tr_id = line_inf[-1].split()[1]
+            # tr_id = line_inf[-1].split()[1]
             tr_id = re.search('transcript_id "(\S+)"', line_inf[-1]).groups()[0]
+            strand = line_inf[6]
+            if strand not in ["+", "-"]:
+                line_inf[6] = "+"
             if tr_id not in tr_dict:
                 tr_dict[tr_id] = {}
                 tr_dict[tr_id]["exon"] = []
                 tr_dict[tr_id]["CDS"] = []
                 tr_dict[tr_id]["transcript"] = []
+            new_out_inf = "\t".join(line_inf)
             if "gene_id" not in line:
                 gene_id = line.strip().split()[-1]
-                out_inf = f"{line.strip()} gene_id {gene_id}\n"
-            tr_dict[tr_id][line_inf[2]].append(out_inf)
+                new_out_inf = f"{line.strip()} gene_id {gene_id}\n"
+            tr_dict[tr_id][line_inf[2]].append(new_out_inf)
     with out_gtf.open("w") as out:
         for transcript in tr_dict:
             for feature in ["transcript", "exon", "CDS"]:
