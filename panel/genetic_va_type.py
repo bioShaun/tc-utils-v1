4b3f3b8e3a42028bb90996177292987e423c9b0f
brightrock <ricekent@163.com>
2024-10-22 17:01:13 +0800

    fix: more than one protein

--- panel/genetic_va_type.py
+++ panel/genetic_va_type.py
@@ -48,7 +48,10 @@ def is_missense_or_nonsynonymous(hgvs_p: str) -> str:
         return "other"
     if "*" in hgvs_p or "?" in hgvs_p:
         return "non-synonymous"
-    protein_a, protein_b = re.split("[0-9]+", hgvs_p.split(".")[1])
+    proteins = re.split("[0-9]+", hgvs_p.split(".")[1])
+    if len(proteins) != 2:
+        return "non-synonymous"
+    protein_a, protein_b = proteins
     if protein_a == protein_b:
         return "synonymous"
     return "non-synonymous"
