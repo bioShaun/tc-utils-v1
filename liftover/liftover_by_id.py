4db96bfe2212384d7d8a7b7261f0b2113baacb92
brightrock <ricekent@163.com>
2025-06-21 15:47:35 +0800

    fix(liftover): standardize outdir parameter naming and update bedtools commands for consistency

--- liftover/liftover_by_id.py
+++ liftover/liftover_by_id.py
@@ -35,13 +35,12 @@ def make_chain(
     chain: Path,
     ref_fa: Path,
     query_fa: Path,
-    out_dir: Path,
+    outdir: Path,
     force: bool = False,
 ) -> None:
     """Make a chain file from a reference and query file."""
     logger.info(f"Generating vcf from ID file: {id_file}")
     vcf: Path = make_id_vcf(id_file, ref_fa, force=force)
-    outdir: Path = id_file.parent
     lift_over_vcf: Path = outdir / f"liftover.{id_file.name}.vcf.gz"
     rejected_vcf: Path = outdir / f"rejected.{id_file.name}.vcf.gz"
     liftover_cmd: str = (
@@ -68,22 +67,22 @@ def make_chain(
     lift_bed["start"] = lift_bed["pos"] - 1
     lift_bed["id"] = lift_bed["chrom"] + "_" + lift_bed["start"].astype(str)
     lift_bed.drop_duplicates(inplace=True)
-    out_dir.mkdir(exist_ok=True, parents=True)
-    raw_bed = out_dir / f"raw.{id_file.stem}.bed"
+    outdir.mkdir(exist_ok=True, parents=True)
+    raw_bed = outdir / f"raw.{id_file.stem}.bed"
     probe_name = id_file.stem
 
-    probe_id_file = out_dir / f"{probe_name}.id"
+    probe_id_file = outdir / f"{probe_name}.id"
     lift_bed.to_csv(probe_id_file, sep="\t", index=False, header=False, columns=["id"])
     lift_bed.to_csv(
         raw_bed, sep="\t", index=False, header=False, columns=["chrom", "start", "pos"]
     )
     ref_fa_idx = f"{ref_fa}.fai"
-    probe_bed = out_dir / f"{probe_name}.bed"
+    probe_bed = outdir / f"{probe_name}.bed"
     sort_cmd = f"bedtools sort -i {raw_bed} -g {ref_fa_idx} > {probe_bed}"
     logger.info(f"Running bedtools sort: {sort_cmd}")
     delegator.run(sort_cmd)
-    snp_calling_bed = out_dir / f"{probe_name}.snpcalling.bed"
-    span_bed_cmd = f"bedtools slop -i {probe_bed} -g {ref_fa_idx} -b 200 | bedtools merge -i - > {snp_calling_bed}"
+    snp_calling_bed = outdir / f"{probe_name}.snpcalling.bed"
+    span_bed_cmd = f"bedtools slop -i {probe_bed} -g {ref_fa_idx} -b 100 | bedtools merge -i - > {snp_calling_bed}"
     logger.info(f"Running bedtools span bed: {span_bed_cmd}")
     delegator.run(span_bed_cmd)
     # rm raw bed
