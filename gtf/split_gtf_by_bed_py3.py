b783ef508e4a3a83cf44dfbd36709cb35a19494b
brightrock <ricekent@163.com>
2025-07-08 10:02:15 +0800

    refactor(gtf): improve GTF splitting logic and remove unused code
    - Enhance the GTF splitting algorithm to handle cases where the start or end position falls within the specified region
    - Remove unused imports and dead code
    - Rename the main script file to remove the '_v2' suffix
    - Create a backup of the previous version with a '_v2' suffix

--- gtf/split_gtf_by_bed_py3.py
+++ gtf/split_gtf_by_bed_py3.py
@@ -1,6 +1,8 @@
+from math import e
 from pathlib import Path
 
 import typer
+from matplotlib.cbook import contiguous_regions
 
 
 def main(bed: Path, gtf: Path):
@@ -28,14 +30,30 @@ def main(bed: Path, gtf: Path):
                 chrom, *_, start, end = output_line[:5]
             except ValueError:
                 raise ValueError(eachline)
+            output_line_cp = output_line[:]
             if chrom in chr_dict:
                 for each_inter in chr_dict[chrom]:
-                    if int(start) >= each_inter[0] and int(end) <= each_inter[1]:
-                        output_line[0] = chr_dict[chrom][each_inter]
-                        output_line[3] = str(int(start) - each_inter[0])
-                        output_line[4] = str(int(end) - each_inter[0])
-            output_str = "\t".join(output_line)
-            print(output_str)
+                    if int(start) > each_inter[1]:
+                        continue
+                    if int(start) >= each_inter[0] and int(start) <= each_inter[1]:
+                        output_line_cp[0] = chr_dict[chrom][each_inter]
+                        output_line_cp[3] = str(int(start) - each_inter[0])
+                        if int(end) <= each_inter[1]:
+                            output_line_cp[4] = str(int(end) - each_inter[0])
+                        else:
+                            if int(each_inter[0]) != 0:
+                                raise ValueError(f"超过染色体长度, {eachline}")
+                            output_line_cp[4] = str(each_inter[1])
+                        output_str = "\t".join(output_line_cp)
+                        print(output_str)
+                    else:
+                        if int(end) > each_inter[0]:
+                            output_line_cp2 = output_line[:]
+                            output_line_cp2[0] = chr_dict[chrom][each_inter]
+                            output_line_cp2[3] = str(0)
+                            output_line_cp2[4] = str(int(end) - each_inter[0])
+                            output_str = "\t".join(output_line_cp2)
+                            print(output_str)
 
 
 if __name__ == "__main__":
