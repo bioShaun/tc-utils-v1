950d7a911e39cc160e2cd17a4d015fbf240c73ed
brightrock <ricekent@163.com>
2025-07-14 11:12:05 +0800

    refactor(gwas): enhance gemma-plot script and update VSCode settings
    - Modify main function to accept data directory and optional output directory
    - Update file path handling for generated plots
    - Add type hint for optional parameters in gemma-plot.py
    - Adjust VSCode settings to disable Python language server

--- gwas/gemma-plot.py
+++ gwas/gemma-plot.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -18,9 +19,13 @@ MODELS = {
 }
 
 
-def main(out_dir: Path, chr_prefix: str = typer.Option(None)):
-    for each_file in out_dir.glob("*assoc.txt"):
+def main(
+    data_dir: Path, out_dir: Optional[Path] = None, chr_prefix: str = typer.Option(None)
+):
+    for each_file in data_dir.glob("*/*assoc.txt"):
+        file_path = each_file.parent
         df = pd.read_table(each_file)
+        plot_dir = file_path if out_dir is None else out_dir
         if chr_prefix:
             df["chr"] = df["chr"].map(lambda x: x.lstrip(chr_prefix))
         for each_test in TESTS:
@@ -28,7 +33,7 @@ def main(out_dir: Path, chr_prefix: str = typer.Option(None)):
             model_name = MODELS[model]
             test_name = TESTS[each_test]
             logger.info(f"Plotting {gt} {model_name} ({test_name})")
-            manhattanplot_png = out_dir / f"{gt}_{model}_{each_test}.manhattan.png"
+            manhattanplot_png = plot_dir / f"{gt}_{model}_{each_test}.manhattan.png"
             plot_df = df.dropna(subset=[f"p_{each_test}"]).copy()
             if not manhattanplot_png.is_file():
                 f, ax = plt.subplots(figsize=(12, 4), facecolor="w", edgecolor="k")
@@ -42,17 +47,22 @@ def main(out_dir: Path, chr_prefix: str = typer.Option(None)):
                     hline_kws={"linestyle": "--", "lw": 1.3},
                     sign_line_cols=["#D62728", "#2CA02C"],
                 )
-                manhattan_plot_prefix = out_dir / f"{gt}_{model}_{each_test}.manhattan"
+                ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+                manhattan_plot_prefix = plot_dir / f"{gt}_{model}_{each_test}.manhattan"
+                plt.tight_layout()
+                # f.subplots_adjust(bottom=0.25)
                 f.savefig(f"{manhattan_plot_prefix}.png", dpi=300)
                 f.savefig(f"{manhattan_plot_prefix}.pdf")
                 plt.close(f)
-            qq_plot_png = out_dir / f"{gt}_{model}_{each_test}.QQ.png"
+            qq_plot_png = plot_dir / f"{gt}_{model}_{each_test}.QQ.png"
             if not qq_plot_png.is_file():
+                plt.figure(figsize=(12, 6))
                 ax = qqplot(
                     data=plot_df[f"p_{each_test}"],
                     title=f"{gt} {model_name} ({test_name})",
                 )
-                qq_plot_prefix = out_dir / f"{gt}_{model}_{each_test}.QQ"
+                ax.title.set_fontsize(8)
+                qq_plot_prefix = plot_dir / f"{gt}_{model}_{each_test}.QQ"
                 plt.savefig(f"{qq_plot_prefix}.png", dpi=300)
                 plt.savefig(f"{qq_plot_prefix}.pdf")
                 plt.close()
