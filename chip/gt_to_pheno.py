19edfa0ebe504023ef60c363f8da1a0d90ba83b1
brightrock <ricekent@163.com>
2025-07-28 16:44:32 +0800

    feat(chip): add gt_to_pheno.py script
    - Implement a new Python script for converting genotype data to phenotype data
    - Add functions for filtering alleles and determining phenotypes based on genotypes
    - Create a main function to process input files and generate output phenotype file
    - Use pandas for data manipulation and excel writer for output

---
+++ chip/gt_to_pheno.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def filter_by_allele(pheno_alleles, gt_alleles):
+    for gt_allele in gt_alleles:
+        if "del" in gt_allele:
+            return "del" in pheno_alleles
+        if "ins" in gt_allele:
+            return "ins" in pheno_alleles
+
+    for gt_allele in gt_alleles:
+        if gt_allele == ".":
+            continue
+        if gt_allele not in pheno_alleles:
+            return False
+    return True
+
+
+def get_gt_pheno(alleles: tuple[str], phenos: tuple[str], gt: str) -> str:
+    if "N" in gt:
+        return "Missing"
+    if "del" in alleles or "ins" in alleles:
+        if "/" in gt:
+            return "Heterozygous"
+        if "del" in alleles:
+            return phenos[alleles.index("del")].capitalize()
+        return phenos[alleles.index("ins")].capitalize()
+    if len(set(gt)) > 1:
+        return "Heterozygous"
+    return phenos[alleles.index(gt[0])].capitalize()
+
+
+def main(gt_file: Path, pheno_file: Path, out_file: Path):
+    gt_df = pd.read_table(gt_file)
+    pheno_df = pd.read_table(pheno_file)
+    gt_allele_df = pheno_df.rename(columns={"chrom": "CHROM", "pos": "POS"}).merge(
+        gt_df
+    )
+    gt_allele_df["gt_consistent"] = gt_allele_df.apply(
+        lambda row: filter_by_allele(
+            (row["ref_allele1"], row["ref_allele2"]), (row["REF"], row["ALT"])
+        ),
+        axis=1,
+    )
+    consistant_df = gt_allele_df[gt_allele_df["gt_consistent"]].copy()
+    sample_list = consistant_df.columns[10:-1].to_list()
+    pheno_df = consistant_df[consistant_df.columns[:4]][
+        ["CHROM", "POS", "trait", "gene"]
+    ].copy()
+    for sample in sample_list:
+        pheno_df[sample] = consistant_df.apply(
+            lambda row: get_gt_pheno(
+                (row["ref_allele1"], row["ref_allele2"]),
+                (row["phenotype1"], row["phenotype2"]),
+                row[sample],
+            ),
+            axis=1,
+        )
+    pheno_df.to_excel(out_file, index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
