b4314ee04fbc36134677f9fe94940d0ebbae5f95
brightrock <ricekent@163.com>
2025-09-08 11:39:27 +0800

    feat(chip): 添加 gt2vcf.py 脚本转换基因型数据为 VCF 格式
    - 新增 gt2vcf.py 文件实现基因型数据到 VCF 格式的转换
    - 添加 make_vcf_header 函数生成 VCF 文件头部信息
    - 实现 main 函数处理数据转换和文件写入
    - 使用 pandas 和 typer 库简化数据处理和命令行参数解析

---
+++ chip/gt2vcf.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pandas as pd
+import typer
+
+
+def make_vcf_header(chr_size: Path) -> str:
+    header = "##fileformat=VCFv4.2\n"
+    with chr_size.open() as f:
+        for line in f:
+            chr, size = line.strip().split("\t")
+            header += f"##contig=<ID={chr},length={size}>\n"
+    return header
+
+
+def main(gt_file: Path, chr_size: Path, vcf_file: Path):
+    gt_df = pd.read_table(gt_file)
+    gt_df["ID"] = gt_df["CHROM"] + "_" + gt_df["POS"].astype(str)
+    gt_df["FILTER"] = "."
+    gt_df["QUAL"] = "."
+    gt_df["INFO"] = "."
+    gt_df["FORMAT"] = "GT"
+    vcf_df = gt_df[
+        [
+            "CHROM",
+            "POS",
+            "ID",
+            "REF",
+            "ALT",
+            "QUAL",
+            "FILTER",
+            "INFO",
+            "FORMAT",
+            *gt_df.columns[4:].tolist(),
+        ]
+    ].copy()
+    vcf_df.rename(columns={"CHROM": "#CHROM"}, inplace=True)
+    header = make_vcf_header(chr_size)
+    with vcf_file.open("w") as f:
+        f.write(header)
+        f.write(gt_df.to_string(index=False, header=False))
+
+
+if __name__ == "__main__":
+    typer.run(main)
