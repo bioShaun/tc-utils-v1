7aa213d22ff0456c0edda5dff46c1f98a876c85c
brightrock <ricekent@163.com>
2025-09-11 11:19:20 +0800

    feat(panel): 整合 minemap 结果并关联 fasta id
    - 新增 extract_fasta_id 函数，用于提取 fasta 文件中的 id
    - 修改 main 函数，整合所有 minimap 文件的结果
    - 将 minimap 结果与 fasta id 进行关联
    - 对结果数据进行填充和类型转换
    - 最后输出整合后的结果文件

--- panel/filter-minimap-probes-large.py
+++ panel/filter-minimap-probes-large.py
@@ -2,17 +2,27 @@ from pathlib import Path
 
 import pandas as pd
 import typer
+from pyfaidx import Fasta
 from tqdm import tqdm
 
 
+def extract_fasta_id(fasta_file: Path) -> pd.DataFrame:
+    fasta_id_list = []
+    for record in Fasta(fasta_file):
+        fasta_id_list.append({"id": record.name})
+    return pd.DataFrame(fasta_id_list)
+
+
 def main(
+    fasta_file: Path,
     minimap_dir: Path,
     out_file: Path,
     max_match_count: int = 1,
     output_all: bool = False,
 ) -> None:
     minimap_files = sorted(minimap_dir.glob("./*"))
-
+    fasta_id_df = extract_fasta_id(fasta_file)
+    id_count_df_list = []
     for n, minimap_file in enumerate(tqdm(minimap_files)):
         minimap_df = pd.read_table(
             minimap_file,
@@ -33,10 +43,15 @@ def main(
         id_count_df["best_match_len"] = (
             id_count_df["best_match_len"].fillna(0).astype(int)
         )
+        id_count_df_list.append(id_count_df)
 
-        mode = "w" if n == 0 else "a"
-        header = n == 0
-        id_count_df.to_csv(out_file, sep="\t", index=False, mode=mode, header=header)
+    id_count_df = pd.concat(id_count_df_list)
+    id_count_df = pd.merge(id_count_df, fasta_id_df, on="id", how="left")
+    id_count_df["minimap_match"].fillna(0, inplace=True)
+    id_count_df["minimap_match"] = id_count_df["minimap_match"].astype(int)
+    id_count_df["best_match_len"].fillna(0, inplace=True)
+    id_count_df["best_match_len"] = id_count_df["best_match_len"].astype(int)
+    id_count_df.to_csv(out_file, sep="\t", index=False)
 
 
 if __name__ == "__main__":
