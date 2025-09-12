703887ef9310bca33055201aa605c9dd396af8b3
brightrock <ricekent@163.com>
2025-09-12 11:33:45 +0800

    feat(chip): 添加获取引物序列侧翼序列的功能
    - 新增 add_flank_seq.py 脚本
    - 通过基因组文件和设计表格生成带有侧翼序列的引物信息
    - 支持自定义侧翼序列长度，默认为 100 bp

---
+++ chip/add_flank_seq.py
@@ -0,0 +1,38 @@
+from functools import partial
+from pathlib import Path
+
+import pandas as pd
+import typer
+from pyfaidx import Fasta
+
+
+def get_flank_seq(
+    genome_fa: Fasta, flank_size: int, chrom: str, pos: int, ref: str, alt: str
+):
+    left = genome_fa[chrom][pos - flank_size : pos - 1]
+    right = genome_fa[chrom][pos : pos + flank_size]
+    return f"{left}[{ref}/{alt}]{right}"
+
+
+def main(
+    design_table: Path,
+    genome_path: Path,
+    flank_size: int = 100,
+):
+    df = pd.read_table(design_table)
+    out_file = design_table.with_suffix(".flank_seq.tsv")
+    genome_fa = Fasta(genome_path, as_raw=True)
+    my_get_flank_seq = partial(
+        get_flank_seq, genome_fa=genome_fa, flank_size=flank_size
+    )
+    df["flank_seq"] = df.apply(
+        lambda x: my_get_flank_seq(
+            chrom=x["chrom"], pos=x["pos"], ref=x["ref"], alt=x["alt"]
+        ),
+        axis=1,
+    )
+    df.to_csv(out_file, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
