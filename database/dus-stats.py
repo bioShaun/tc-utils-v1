b33c6b0d75cf3bb033272d8e5cf357e7e42ad3ba
brightrock <ricekent@163.com>
2025-09-21 17:11:24 +0800

    feat(database): 将统计结果输出格式从CSV改为Excel
    修改了dus-stats.py脚本中的输出方法，将psc_df数据框的输出格式从CSV改为Excel格式，
    以提供更好的数据展示和兼容性。

--- database/dus-stats.py
+++ database/dus-stats.py
@@ -54,7 +54,7 @@ def sampleStats(bcfstats: Path, out_file: Path) -> None:
     psc_df["REF_RATIO"] = psc_df["REF"] / psc_df["Total"]
     psc_df["ALT_RATIO"] = psc_df["ALT"] / psc_df["Total"]
     psc_df["HET_RATIO"] = psc_df["HET"] / psc_df["Total"]
-    psc_df.to_csv(
+    psc_df.to_excel(
         out_file, sep="\t", index=False, columns=OUT_COLUMNS, float_format="%.4f"
     )
 
