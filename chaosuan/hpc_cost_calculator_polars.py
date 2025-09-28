4abb385ad3bf21050950460930001dc98cdffd15
brightrock <ricekent@163.com>
2025-09-28 15:03:21 +0800

    fix(hpc_cost_calculator_polars): 修改统计数据保存格式为Excel
    将最终统计数据的保存方式从CSV格式更改为Excel格式，以提供更好的数据兼容性和可读性。

--- chaosuan/hpc_cost_calculator_polars.py
+++ chaosuan/hpc_cost_calculator_polars.py
@@ -167,7 +167,9 @@ def main(stats_dir: Path, summary_filename: Path, prefix: Optional[str] = None):
 
     # Save the final statistics to a CSV file
     logger.info(f"正在将最终统计数据写入 {summary_filename}")
-    final_stats_df.write_csv(summary_filename, separator="\t")
+    # final_stats_df.write_csv(summary_filename, separator="\t")
+    out_final_stats_df = final_stats_df.to_pandas()
+    out_final_stats_df.to_excel(summary_filename)
     logger.info("处理完成。")
 
 
